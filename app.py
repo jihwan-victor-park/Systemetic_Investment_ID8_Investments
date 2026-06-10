342ea955736cbe37847793f780382eca564c20f9
Oscar Varas Hiza <121910671+ocachin@users.noreply.github.com>
2026-06-10 13:09:04 -0400

    Update 'New Investors' field mapping

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ FIELD_MAP = {
     'Series':              ('series',           'select'),
     'Description':         ('description',      'text'),
     'Lead/Sole Investors': ('lead_investors',    'text'),
-    'New Investors':       ('new_investors',     'text'),
+    'New Investors':       ('new_investors_7',   'text'),
     'Deal Size':           ('deal_size',         'currency'),
     'Post Valuation':      ('post_valuation',    'currency'),
     'Revenue':             ('revenue',           'currency'),
